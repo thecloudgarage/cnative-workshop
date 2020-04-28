ae0e0bea3c2461b4c53bbc87941294612babfa57
thecloudgarage <39495790+thecloudgarage@users.noreply.github.com>
2020-04-28 23:40:40 +0530

    Update route53-pks-api.sh

--- common-scripts/route53-pks-api.sh
+++ common-scripts/route53-pks-api.sh
@@ -1,1 +1,20 @@
-aws --region us-east-1 ec2 describe-instances --filters 'Name=private-ip-address,Values=10.0.1.11' --query 'Reservations[*].Instances[*].[PublicIpAddress]' --output text
+#!/bin/sh
+
+if [ -z "$1" ]; then 
+    echo "IP not given...trying EC2 metadata...";
+    IP=$( aws --region us-east-1 ec2 describe-instances --filters 'Name=private-ip-address,Values=10.0.1.11' --query 'Reservations[*].Instances[*].[PublicIpAddress]' --output text )  
+else 
+    IP="$1" 
+fi 
+echo "IP to update: $IP"
+
+HOSTED_ZONE_ID=$( aws route53 list-hosted-zones-by-name | jq --arg name "thecloudgarage.com." -r '.HostedZones | .[] | select(.Name=="\($name)") | .Id' | sed -r 's/.{12}//' )
+echo "Hosted zone being modified: $HOSTED_ZONE_ID"
+
+INPUT_JSON=$( cat ./route53-pks-api.json | sed "s/127\.0\.0\.1/$IP/" )
+
+INPUT_JSON="{ \"ChangeBatch\": $INPUT_JSON }"
+
+aws route53 change-resource-record-sets --hosted-zone-id "$HOSTED_ZONE_ID" --cli-input-json "$INPUT_JSON"
+
+
