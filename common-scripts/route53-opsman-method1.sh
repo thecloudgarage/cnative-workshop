4e85aff17655d8dfa786e4f73abddeae7e5c9660
thecloudgarage <39495790+thecloudgarage@users.noreply.github.com>
2020-04-29 00:26:15 +0530

    Update route53-opsman-method1.sh

--- common-scripts/route53-opsman-method1.sh
+++ common-scripts/route53-opsman-method1.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-if [ -z "$1" ]; then 
+if [ -z "$1" ]; then
     echo "IP not given...trying EC2 metadata...";
-    IP=$( curl http://169.254.169.254/latest/meta-data/public-ipv4 )  
-else 
-    IP="$1" 
-fi 
+    IP=$( curl http://169.254.169.254/latest/meta-data/public-ipv4 )
+else
+    IP="$1"
+fi
 echo "IP to update: $IP"
 
 HOSTED_ZONE_ID=$( aws route53 list-hosted-zones-by-name | jq --arg name "thecloudgarage.com." -r '.HostedZones | .[] | select(.Name=="\($name)") | .Id' | sed -r 's/.{12}//' )
+
 echo "Hosted zone being modified: $HOSTED_ZONE_ID"
 
 INPUT_JSON=$( cat ./route53-opsman.json | sed "s/127\.0\.0\.1/$IP/" )
