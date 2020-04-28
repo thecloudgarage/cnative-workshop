9a34c7dc7a395a580ac9e7d3f8078474884e99ae
thecloudgarage <39495790+thecloudgarage@users.noreply.github.com>
2020-04-29 00:28:43 +0530

    Update route53-pks-api.sh

--- common-scripts/route53-pks-api.sh
+++ common-scripts/route53-pks-api.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 
-if [ -z "$1" ]; then 
-    echo "IP not given...trying EC2 metadata...";
-    IP=$( aws --region us-east-1 ec2 describe-instances --filters 'Name=private-ip-address,Values=10.0.1.11' --query 'Reservations[*].Instances[*].[PublicIpAddress]' --output text )  
-else 
-    IP="$1" 
-fi 
+export uaaToken=$( sudo curl -s -k -H 'Accept: application/json;charset=utf-8' -d 'grant_type=password' -d 'username=admin' -d 'password=admin' \
+  -u 'opsman:' https://localhost/uaa/oauth/token |  jq --raw-output '.access_token' )
+
+export pksguid=$( curl "https://localhost/api/v0/staged/products" -k \
+  -X GET \
+  -H 'Authorization: Bearer '"$uaaToken"'' \
+  | jq --raw-output '.[] | select(.installation_name|test("pivotal-container-service.")) | .guid' )
+
+echo "PKS GU-ID is: $pksguid"
+
+if [ -z "$1" ]; then
+    echo "IP not given...trying to retrieve PKS API Public IP...";
+
+    IP=$( aws --region us-east-1 ec2 describe-instances --filters 'Name=tag:deployment,Values='"$pksguid"'' \
+      --query 'Reservations[*].Instances[*].[PublicIpAddress]' --output text )
+else
+    IP="$1"
+fi
 echo "IP to update: $IP"
 
 HOSTED_ZONE_ID=$( aws route53 list-hosted-zones-by-name | jq --arg name "thecloudgarage.com." -r '.HostedZones | .[] | select(.Name=="\($name)") | .Id' | sed -r 's/.{12}//' )
+
 echo "Hosted zone being modified: $HOSTED_ZONE_ID"
 
 INPUT_JSON=$( cat ./route53-pks-api.json | sed "s/127\.0\.0\.1/$IP/" )
@@ -16,5 +29,3 @@ INPUT_JSON=$( cat ./route53-pks-api.json | sed "s/127\.0\.0\.1/$IP/" )
 INPUT_JSON="{ \"ChangeBatch\": $INPUT_JSON }"
 
 aws route53 change-resource-record-sets --hosted-zone-id "$HOSTED_ZONE_ID" --cli-input-json "$INPUT_JSON"
-
-
