5242df46c8de1a9ea8b131d2cef805fb6549187e
thecloudgarage <39495790+thecloudgarage@users.noreply.github.com>
2020-04-28 12:40:27 +0530

    Update and rename pks-find-guid.sh to pks-login-from-opsman.sh

---
+++ common-scripts/pks-login-from-opsman.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export uaaToken=$( sudo curl -s -k -H 'Accept: application/json;charset=utf-8' -d 'grant_type=password' -d 'username=admin' -d 'password=admin' \
+  -u 'opsman:' https://localhost/uaa/oauth/token |  jq --raw-output '.access_token' )
+
+export pksguid=$( curl "https://localhost/api/v0/staged/products" -k \
+  -X GET \
+  -H 'Authorization: Bearer '"$uaaToken"'' \
+  | jq --raw-output '.[] | select(.installation_name|test("pivotal-container-service.")) | .guid' )
+
+export uaaadminpassword=$( sudo curl \
+  "https://opsman.thecloudgarage.com/api/v0/deployed/products/$pksguid/credentials/.properties.uaa_admin_password \
+  -k -H 'Authorization: bearer '"$uaaToken"'' )
+  
+pks login -a api.pks.aws.thecloudgarage.com -u admin -p $uaaadminpassword -k
