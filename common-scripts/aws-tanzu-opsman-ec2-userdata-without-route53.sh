e841136078ccd2897e09563660a626d60764d10a
thecloudgarage <39495790+thecloudgarage@users.noreply.github.com>
2020-05-06 19:03:27 +0530

    Update aws-tanzu-opsman-ec2-userdata-without-route53.sh

--- common-scripts/aws-tanzu-opsman-ec2-userdata-without-route53.sh
+++ common-scripts/aws-tanzu-opsman-ec2-userdata-without-route53.sh
@@ -9,7 +9,7 @@ if [ -z "$1" ]; then
 	    fi
 	    echo "IP to update: $IP"
 #ADD A LOCAL ENTRY FOR DNS RESOLUTION TILL ACTUAL OPS MAN DNS MAPPINGS ARE DONE
-sudo echo '"$IP" tanzu-opsman.aws.thecloudgarage.com' >> /etc/hosts
+sudo sed "\$a$IPone tanzu-opsman.aws.thecloudgarage.com" /etc/hosts > /etc/hosts.temp && mv /etc/hosts.temp /etc/hosts
 sudo git clone https://github.com/thecloudgarage/cnative-workshop.git
 cd /home/ubuntu/cnative-workshop/common-scripts/
 sudo sed 's/your-legacy-api-token/Ex58-tDkfHxszQQRNdKf/g' aws-tanzu-opsman-deploy.sh > temp.sh && mv temp.sh aws-tanzu-opsman-deploy.sh
